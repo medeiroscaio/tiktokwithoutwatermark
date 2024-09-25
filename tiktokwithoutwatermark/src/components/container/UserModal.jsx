2b1ce6712a3624613c7c34ad6233414d853f532a
medeiroscaio <caiovictor.medeiros50@gmail.com>
2024-09-25 08:18:39 -0300

    first logic initialized

--- tiktokwithoutwatermark/src/components/container/UserModal.jsx
+++ tiktokwithoutwatermark/src/components/container/UserModal.jsx
@@ -1,10 +1,44 @@
 import { useState } from "react";
 import "./UserModal.css";
+import axios from "axios";
 
 function UserModal() {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
-  const [count, setCount] = useState(0);
+  const [downloadLink, setDownloadLink] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleLink = async () => {
+    if (!url) {
+      alert("TikTok invalid link");
+      return;
+    }
+
+    setLoading(true);
+    setError(null);
+
+    const options = {
+      method: "GET",
+      url: "https://tiktok-video-no-watermark2.p.rapidapi.com/",
+      params: {
+        url: url,
+        hd: "1",
+      },
+      headers: {
+        "x-rapidapi-key": "7effb6e978msh3787522182c0cf7p185c05jsn320426c6b770",
+        "x-rapidapi-host": "tiktok-video-no-watermark2.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      setDownloadLink(response.data.data.play);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="container">
@@ -19,7 +53,16 @@ function UserModal() {
             setUrl(e.target.value);
           }}
         />
-        <button>Download your video without watermark</button>
+        <button onClick={handleLink}>
+          Download your video without watermark
+        </button>
+        {downloadLink && (
+          <button>
+            <a href={downloadLink} target="_blank" rel="noopener noreferrer">
+              Click here to download
+            </a>
+          </button>
+        )}
       </div>
     </div>
   );
